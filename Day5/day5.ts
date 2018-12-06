38aa53c8e251c2eb8a9a11932403b4bd722d86f8
Steven Shipsey <sshipsey@sidearmsports.com>
2018-12-05 19:04:21 -0500

    Day 5, was a very easy one surprisingly

---
+++ Day5/day5.ts
@@ -0,0 +1,41 @@
+import * as _ from 'lodash';
+import { promisify } from 'util';
+import { readFile } from 'fs';
+
+const part1 = (reaction: string) => {
+  let idx = 1;
+  while (idx < reaction.length) {
+    let currentChar = reaction[idx];
+    let previousChar = reaction[idx - 1];
+    if (reacts(currentChar, previousChar)) {
+      reaction = reaction
+        .slice(0, idx - 1)
+        .concat(reaction.slice(idx + 1, reaction.length));
+      idx = 1;
+    } else {
+      idx += 1;
+    }
+  }
+  return reaction.length;
+};
+
+const part2 = (input: string) => {
+  let minLen = Infinity;
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  for (let char of alphabet) {
+    const newPolymer = input
+      .split(char)
+      .join('')
+      .split(char.toUpperCase())
+      .join('');
+    minLen = Math.min(part1(newPolymer), minLen);
+  }
+  return minLen;
+};
+const reacts = (a: string, b: string) =>
+  Math.abs(a.charCodeAt(0) - b.charCodeAt(0)) === 32;
+
+(async () => {
+  const input = await promisify(readFile)('Day5/input.txt', 'utf8');
+  console.log(part1(input), part2(input));
+})();
